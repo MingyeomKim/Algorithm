0744afa280143bb0c113c9cb4fd075f40ef02b14
MGKIM01 <67851124+MGKIM01@users.noreply.github.com>
2020-12-31 15:56:15 +0900

    Create 1931회의실배정.cpp
    틀만 짬

---
+++ 1931회의실배정.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int N; cin >> N; //회의 개수
+	int* start = new int[N];
+	int* end = new int[N];
+	for (int i = 0; i < N; i++)
+	{
+		cin >> start[i] >> end[i];
+	}
+	//회의의 최대 개수
+	int count = 0;
+	for (int i = 0;i < N;i++)
+	{
+
+	}
+}
