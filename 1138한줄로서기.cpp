79601ad18cbd4c61dd60f8b063172593427fe2a2
MGKIM01 <67851124+MGKIM01@users.noreply.github.com>
2020-12-31 16:22:02 +0900

    Update 1138한줄로서기.cpp
    해결 코드

--- 1138한줄로서기.cpp
+++ 1138한줄로서기.cpp
@@ -1,3 +1,46 @@
+/* #include <iostream>
+using namespace std;
+
+int main()
+{
+	int N; cin >> N;
+	int* info = new int[N]; 
+	for (int i = 0; i < N; i++) 
+	{
+		cin >> info[i];
+	}
+	int* line = new int[N] {0,};
+	//line = {0};  // 오류 원인 1
+
+	line[info[0]] = 1; 
+	line[0] = N; //오류 원인 2
+	int count = 0;
+	for (int i = 1; i < N -1; i++)
+	{
+		for (int j = 0; j < N; j++)
+		{
+			if (line[j] == 0)
+			{
+				count = 0; //오류 원인3
+				count++;
+				if (count == info[i])
+				{
+					line[j] = i + 1;
+					break;
+				}
+			}
+			
+		}
+	}
+
+	for (int i = 0;i < N;i++)
+	{
+		cout << line[i] << " ";
+	}
+	return 0;
+}
+
+*/
 #include <iostream>
 using namespace std;
 
@@ -9,29 +52,27 @@ int main()
 	{
 		cin >> info[i];
 	}
-	int* line = new int[N] {0,};
+	int* line = new int[N] {0, };
 	//line = {0}; 이렇게 초기화해주니 에러뜸
 
-	//line[info[1] + 1] = 1
-	//line[info[2] + 1] = 2
 	line[info[0]] = 1; //info[0]+1을 인덱스로 넣음
-	line[0] = N;
+	//line[0] = N; 
 	int count = 0;
-	for (int i = 1; i < N -1; i++)
+	for (int i = 1; i < N; i++)
 	{
+		count = 0; 
 		for (int j = 0; j < N; j++)
 		{
 			if (line[j] == 0)
 			{
-				count = 0;
 				count++;
-				if (count == info[i])
+				if (count == info[i] + 1)
 				{
 					line[j] = i + 1;
 					break;
 				}
 			}
-			
+
 		}
 	}
 
