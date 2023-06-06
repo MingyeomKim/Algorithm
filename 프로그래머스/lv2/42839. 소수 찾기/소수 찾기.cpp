0fe15e390317c0c3cf864375919743ef341987a0
김민겸 <67851124+Mingyum-Kim@users.noreply.github.com>
2023-06-06 23:27:49 +0900

    [level 2] Title: 소수 찾기, Time: 5.02 ms, Memory: 13.2 MB -BaekjoonHub

--- 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.cpp
+++ 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.cpp
@@ -1,33 +1,9 @@
 #include <string>
 #include <vector>
-#include <string>
-#include <algorithm>
-#include <cstring>
-#include <iostream>
 
 using namespace std;
 
-bool isPrime(int n){
-    if(n == 0 || n == 1) return false;
-    for(int i = 2; i < n; i++){
-        if(n % i == 0) return false; 
-    }
-    return true; 
-}
-
-bool visited[10000000]; 
 int solution(string numbers) {
     int answer = 0;
-    memset(visited, false, sizeof(visited)); 
-    sort(numbers.begin(), numbers.end()); 
-    int len = numbers.length(); 
-    do{
-        for(int i = 1; i <= len; i++){
-            int n = stoi(numbers.substr(0, i)); 
-            if(isPrime(n) && !visited[n]) answer++; 
-            visited[n] = true; 
-        }
-    } while(next_permutation(numbers.begin(), numbers.end()));
-    
     return answer;
 }
