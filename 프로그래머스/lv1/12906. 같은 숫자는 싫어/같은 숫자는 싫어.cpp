32b644318813ff2600f68bc267569172697b2dfa
김민겸 <67851124+Mingyum-Kim@users.noreply.github.com>
2023-06-11 16:45:51 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 118.71 ms, Memory: 105 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.cpp
@@ -0,0 +1,12 @@
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr) 
+{
+    arr.erase(unique(arr.begin(), arr.end()), arr.end()); 
+
+    return arr;
+}
