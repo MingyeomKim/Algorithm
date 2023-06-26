7136cc5f742c15a04a9a9d995747296344013e68
MingyeomKim <mingyum119@naver.com>
2023-06-26 10:38:46 +0900

    [230626] 17086 아기상어 2

---
+++ 백준/BFS/17086 아기상어2.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <cstring>
+#include <tuple>
+using namespace std; 
+
+int n, m; 
+int board[51][51]; 
+int dist[51][51]; 
+
+int dy[] = { 0, 0, 1, -1, -1, -1, 1, 1 }; 
+int dx[] = { 1, -1, 0, 0, -1, 1, -1, 1 }; 
+
+int isOut(int y, int x) {
+	return y < 0 || y >= n || x < 0 || x >= m; 
+}
+
+int bfs(int y, int x) {
+	memset(dist, -1, sizeof(dist)); 
+	queue<pair<int, int>> q; 
+	dist[y][x] = 0; 
+	q.push(make_pair(y, x)); 
+
+	while (!q.empty()) {
+		tie(y, x) = q.front(); q.pop();
+		for (int direction = 0; direction < 8; direction++) {
+			int ny = y + dy[direction], nx = x + dx[direction]; 
+			if (isOut(ny, nx) || dist[ny][nx] != -1) continue; 
+			dist[ny][nx] = dist[y][x] + 1; 
+			if (board[ny][nx] == 1)
+				return dist[ny][nx]; 
+			else
+				q.push(make_pair(ny, nx)); 
+		}
+	}
+}
+
+int main() {
+	cin >> n >> m; 
+	vector<pair<int, int>> v; 
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < m; j++) {
+			cin >> board[i][j];
+			if (board[i][j] == 0)
+				v.emplace_back(i, j); 
+		}
+	int max = 0; 
+	for (auto p : v) {
+		int y = p.first, x = p.second; 
+		int cand = bfs(y, x); 
+		if (cand > max) max = cand; 
+	}
+	cout << max << endl;
+	return 0; 
+}
