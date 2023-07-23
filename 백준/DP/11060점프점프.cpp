69cef25359fc3999743b091ac5e7915ba0ac6c8f
MingyeomKim <mingyum119@naver.com>
2023-07-23 22:36:40 +0900

    [230723] 11060 점프 점프 #2

--- 백준/DP/11060점프점프.cpp
+++ 백준/DP/11060점프점프.cpp
@@ -2,6 +2,8 @@
 #include <cstring>
 using namespace std; 
 
+/*
+
 int a[1001]; 
 int ret[1001]; 
 
@@ -22,4 +24,38 @@ int main() {
 	}
 	cout << ret[n -1] << endl;
 	return 0; 
+}
+
+*/
+
+
+int board[1001];
+int cache[1001]; // i번째 위치에서 n위치까지 가는 데 점프해야하는 최소 횟수
+int n;
+
+int solve(int index) {
+	if (index >= n) return INF;
+	if (index == n - 1) return 0;
+	if (cache[index] != -1) return cache[index];
+
+	cache[index] = INF;
+	for (int jump = 1; jump <= board[index]; jump++) {
+		if (index + jump >= n) break;
+		cache[index] = min(cache[index], solve(index + jump) + 1);
+	}
+	return cache[index];
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cout.tie(NULL); cin.tie(NULL);
+
+	cin >> n;
+	for (int i = 0; i < n; i++)
+		cin >> board[i];
+	memset(cache, -1, sizeof(cache));
+	int ret = solve(0);
+	if (ret == INF) cout << -1 << endl;
+	else cout << ret << endl;
+	return 0;
 }
