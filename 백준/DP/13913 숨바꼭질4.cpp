2be7c1a355d384d0292f2149d658c4152fee7c06
MingyeomKim <mingyum119@naver.com>
2024-01-26 11:13:15 +0800

    [240126] 13913 숨바꼭질3

--- 백준/DP/13913 숨바꼭질4.cpp
+++ 백준/DP/13913 숨바꼭질4.cpp
@@ -1,28 +1,62 @@
 #include <iostream>
+#include <queue>
 #include <cstring>
-#include <cmath>
-using namespace std;
+#include <vector>
+#include <algorithm>
+using namespace std; 
 
-int dp[100001];
-int n, k;
+int dist[100001];
+int trace[100001]; // trace[i] : i에 도달하기 전에 거치는 위치
 
-void compare(int& next, int current) {
-	if (next == -1 || next > current + 1) {
-		next = current + 1;
+void bfs(int start, int end) {
+	queue<int> q; 
+	q.push(start);
+	dist[start] = 0;
+	while (!q.empty()) {
+		int here = q.front(); 
+		q.pop(); 
+		int nexts[3] = { here + 1, here - 1, here * 2 }; 
+		for (int i = 0; i < 3; i++) {
+			int next = nexts[i];
+			if (next < 0 || next > 100001) continue;
+			if (dist[next] != -1) continue;
+			if (dist[next] < dist[here] + 1) {
+				dist[next] = dist[here] + 1;
+				trace[next] = here;
+				q.push(next);
+			}
+		}
 	}
 }
 
 int main() {
-	cin >> n >> k;
-	memset(dp, -1, sizeof(dp));
-	dp[n] = 0;
-	for (int i = 0; i <= abs(n - k); i++) {
-		int current = i + n;
-		compare(dp[current + 1], dp[current]);
-		compare(dp[current - 1], dp[current]);
-		compare(dp[2 * current], dp[current]);
+	ios_base::sync_with_stdio(false); 
+	cout.tie(NULL);
+	int n, k; cin >> n >> k;
+
+	if (n == k) {
+		cout << 0 << endl;
+		cout << n << endl;
+		return 0;
 	}
 
-	cout << dp[k] << endl;
+	memset(dist, -1, sizeof(dist));
+	bfs(n, k);
+	cout << dist[k] << endl;
+
+	vector<int> v;
+	v.push_back(k);
+	int here = trace[k];
+	while (true) {
+		v.push_back(here);
+		if (here == n) {
+			break;
+		}
+		here = trace[here];
+	}
+	reverse(v.begin(), v.end()); 
+	for (int x : v) {
+		cout << x << " ";
+	} cout << endl;
 	return 0;
 }
