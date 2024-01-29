22a55451be4f09bc87de5c7627eaac36a3d05fbd
MingyeomKim <mingyum119@naver.com>
2024-01-29 15:01:33 +0900

    [240129] 3085 사탕 게임

--- 백준/Implementation/3085 사탕 게임.cpp
+++ 백준/Implementation/3085 사탕 게임.cpp
@@ -1,54 +1,61 @@
 #include <iostream>
 #include <cstring>
-using namespace std;
+using namespace std; 
 
 char board[51][51];
-int dy[4] = { -1, 1, 0, 0 };
+int dy[4] = { -1, 1, 0, 0 }; 
 int dx[4] = { 0, 0, 1, -1 };
 
-int count(int n) {
+int n;
+bool isOut(int y, int x) {
+	return y < 0 || y >= n || x < 0 || x >= n;
+}
+
+void swap(int y, int x, int ny, int nx) {
+	int temp = board[y][x]; 
+	board[y][x] = board[ny][nx];
+	board[ny][nx] = temp; 
+}
+
+int count() {
 	int max = 0;
-	for (int i = 0; i < n; i++) {
-		char x = board[i][0];
-		int count = 1;
-		for (int j = 0; j < n; j++) {
-			if (board[i][j] != x) {
-				x = board[i][j];
-				if (count > max) {
-					max = count;
-				}
-				count = 1;
+	for (int y = 0; y < n; y++) {
+		char now = board[y][0];
+		int cnt = 1;
+		for (int x = 1; x < n; x++) {
+			if (now != board[y][x]) {
+				if (max < cnt) max = cnt;
+				cnt = 1;
+				now = board[y][x];
 			}
 			else {
-				count++;
+				cnt++;
 			}
 		}
+		if (max < cnt) max = cnt;
 	}
-	for (int j = 0; j < n; j++) {
-		char x = board[0][j];
-		int count = 1;
-		for (int i = 0; i < n; i++) {
-			if (board[i][j] != x) {
-				x = board[i][j];
-				if (count > max) {
-					max = count;
-				}
-				count = 1;
+	for (int x = 0; x < n; x++) {
+		char now = board[0][x]; 
+		int cnt = 1; 
+		for (int y = 1; y < n; y++) {
+			if (now != board[y][x]) {
+				if (max < cnt) max = cnt; 
+				cnt = 1; 
+				now = board[y][x]; 
 			}
 			else {
-				count++;
+				cnt++;
 			}
 		}
+		if (max < cnt) max = cnt;
 	}
 	return max;
 }
 
-bool isOut(int y, int x, int n) {
-	return y < 0 || y >= n || x < 0 || x >= n;
-}
-
 int main() {
-	int n; cin >> n;
+	ios_base::sync_with_stdio(false); 
+	cin.tie(NULL);
+	cin >> n;
 	for (int i = 0; i < n; i++) {
 		string s; cin >> s;
 		for (int j = 0; j < n; j++) {
@@ -57,18 +64,15 @@ int main() {
 	}
 
 	int ret = 0;
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			char here = board[i][j];
+	for (int y = 0; y < n; y++) {
+		for (int x = 0; x < n; x++) {
 			for (int k = 0; k < 4; k++) {
-				if (isOut(i + dy[k], j + dx[k], n)) continue;
-				char next = board[i + dy[k]][j + dx[k]];
-				board[i][j] = next;
-				board[i + dy[k]][j + dx[k]] = here;
-				int cand = count(n);
-				if (cand > ret) ret = cand;
-				board[i][j] = here;
-				board[i + dy[k]][j + dx[k]] = next;
+				int ny = y + dy[k], nx = x + dx[k];
+				if(isOut(ny, nx)) continue;
+				swap(y, x, ny, nx);
+				int cand = count();
+				if (ret < cand) ret = cand;
+				swap(y, x, ny, nx); 
 			}
 		}
 	}
