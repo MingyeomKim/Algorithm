14ad7ae069100bb61ed2feffa99428622813b7c0
MingyeomKim <67851124+MingyeomKim@users.noreply.github.com>
2024-02-05 13:48:45 +0900

    [240205] 1912 연속합

--- 백준/DP/1912 연속합.cpp
+++ 백준/DP/1912 연속합.cpp
@@ -1,43 +1,24 @@
 #include <iostream>
-using namespace std;
+#include <vector>
+using namespace std; 
 
-int numbers[100001];
-int summary[100001];
-int ret = -1001;
-void solve(int start, int end) {
-	int sum = 0;
-	if (start == 0) {
-		sum = summary[end];
-	}
-	else {
-		sum = summary[end] - summary[start - 1];
-	}
-	if (ret < sum) {
-		ret = sum;
-	}
-	if (start + 1 == end) return;
-
-	for (int middle = start + 1; middle < end; middle++) {
-		solve(start, middle);
-		solve(middle, end);
-	}
-}
+int dp[100001];
 
 int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
 	int n; cin >> n;
+	vector<int> number(n);
 	for (int i = 0; i < n; i++) {
-		cin >> numbers[i];
-		if (ret < numbers[i]) {
-			ret = numbers[i];
-		}
-		if (i == 0) {
-			summary[i] = numbers[i];
-		}
-		else {
-			summary[i] = summary[i - 1] + numbers[i];
-		}
+		cin >> number[i];
 	}
-	solve(0, n - 1);
+
+	int ret = number[0];
+	dp[0] = number[0];
+	for (int i = 1; i < n; i++) {
+		dp[i] = max(dp[i - 1] + number[i], number[i]);
+		ret = max(ret, dp[i]);
+	}
 	cout << ret << endl;
 	return 0;
 }
