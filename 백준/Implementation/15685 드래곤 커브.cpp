e2f713acfc1001c390557db24d76f8ab64807429
MingyeomKim <67851124+MingyeomKim@users.noreply.github.com>
2024-02-05 12:58:52 +0900

    [240205] 15685 드래곤 커브

--- 백준/Implementation/15685 드래곤 커브.cpp
+++ 백준/Implementation/15685 드래곤 커브.cpp
@@ -6,20 +6,21 @@ using namespace std;
 int dy[4] = { 0, -1, 0, 1 };
 int dx[4] = { 1, 0, -1, 0 };
 
-bool dragon[102][102]; // 드래곤 커브의 모양을 저장하는 배열
+bool dragon[102][102]; 
 
-// 드래곤 커브를 실제로 그리고 한 칸의 네 꼭짓점 중에서 드래곤 커부의 일부에 속하는 꼭짓점의 개수를 저장하는 로직
 int main() {
+	ios_base::sync_with_stdio(false); 
+	cin.tie(NULL); 
 	int n; cin >> n;
-	vector<pair<int, int>> v;
 	for (int i = 0; i < n; i++) {
+		vector<pair<int, int>> v;
 		int x, y; cin >> x >> y;
 		int d, g; cin >> d >> g;
 		v.push_back(make_pair(x, y));
 		int ny = y + dy[d], nx = x + dx[d];
-		v.push_back(make_pair(nx, ny)); // 0세대
+		v.push_back(make_pair(nx, ny));
 
-		for (int j = 0; j < g; j++) { // 1 세대 ~ g 세대
+		for (int j = 0; j < g; j++) { 
 			auto back = v.back();
 			int a = back.first, b = back.second;
 			int size = v.size();
