53492734ef5bfefb457b7fcb60852174a77ad3f4
MingyeomKim <mingyum119@naver.com>
2024-02-07 10:33:57 +0900

    [240207] 12851 숨바꼭질2

--- 백준/BFS/12851 숨바꼭질2.cpp
+++ 백준/BFS/12851 숨바꼭질2.cpp
@@ -1,36 +1,37 @@
 #include <iostream>
-#include <cstring>
 #include <queue>
-using namespace std;
+#include <tuple>
+using namespace std; 
 
-int route[100001]; // route[i] : i번째 위치에 올 수 있는 방법의 수
-int dist[100001]; // dist[i] : i번째 위치에 가는 가장 빠른 시간
+bool check[100001]; 
+int min_time = 987654321;
+int cnt = 0;
 
 int main() {
 	int n, k; cin >> n >> k;
-	memset(dist, -1, sizeof(dist));
-	queue<int> q;
-	q.push(n);
-	dist[n] = 0;
+	queue<pair<int, int>> q;
+	q.push(make_pair(n, 0));
 	while (!q.empty()) {
-		int here = q.front(); q.pop();
+		int here, time;
+		tie(here, time) = q.front(); 
+		q.pop();
+		
+		check[here] = true;
+		if (min_time < time) break; // 이미 최소를 벗어남
+		if (here == k) {
+			min_time = time;
+			cnt++;
+			continue; // 이미 목적지에 도달하면, 그 다음으로 이동하는 건 모두 최소가 될 수 없음
+		}
 		int nexts[3] = { here + 1, here - 1, here * 2 };
 		for (int i = 0; i < 3; i++) {
 			int next = nexts[i];
-			if (next < 0 || next > 100000) continue;
-			q.push(next);
-			dist[next] = dist[here] + 1;
+			if (next < 0 || next > 100000 || check[next]) continue;
+			q.push(make_pair(next, time + 1));
+		
 		}
 	}
-	cout << dist[k] << endl;
-	cout << route[dist[k]] << endl;
+	cout << min_time << endl; 
+	cout << cnt << endl;
 	return 0;
 }
-
-/*
-5 10 9 18 17
-5 4 8 16 17
-
-방문한 노드는 다시 방문하지 않으니까 여러 경로로 17에 도달할 수 있어도 실제로 하나만 탐색된다. 같은 경로라도 짚고 넘어가야함 ?
-근데 BFS니까 한번 지나가면 그 다음부터는 최소가 아님. 같은 레벨에서도
-*/
