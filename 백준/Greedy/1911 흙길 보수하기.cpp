2dd606bdc46db1b23ae06a971b568b5eb7b1cf04
MingyeomKim <67851124+MingyeomKim@users.noreply.github.com>
2024-02-14 07:02:35 +0900

    [240214] 1911 흙길 보수하기

--- 백준/Greedy/1911 흙길 보수하기.cpp
+++ 백준/Greedy/1911 흙길 보수하기.cpp
@@ -4,48 +4,28 @@
 using namespace std;
 
 int main() {
+	ios_base::sync_with_stdio(false); 
+	cin.tie(NULL);
 	int n, l; cin >> n >> l;
-	int current = 0;
+	int start = -1, end = -1;
 	int ret = 0;
 	vector<pair<int, int>> v(n);
 	for (int i = 0; i < n; i++) {
-		int start, end; cin >> start >> end;
-		v[i].first = start, v[i].second = end;
+		int a, b; cin >> a >> b;
+		v[i].first = a, v[i].second = b;
 	}
 	sort(v.begin(), v.end());
 	for (int i = 0; i < n; i++) {
-		int start = v[i].first, end = v[i].second;
-		if (current + l >= end) {
-			continue;
-		}
-		else if (current >= start && current < end) {
-			while (true) {
-				if (current + l >= end) {
-					ret++;
-					break;
-				}
-				current = current + l;
+		int a = v[i].first, b = v[i].second;
+		while (end < b) {
+			if (a > end) {
+				start = a;
+				end = a + l;
 				ret++;
 			}
-		}
-		else if (current + l >= start && current + l < end) {
-			while (true) {
-				if (current + l >= end) {
-					ret++;
-					break;
-				}
-				current = current + l;
-				ret++;
-			}
-		}
-		else {
-			current = start;
-			while (true) {
-				if (current + l >= end) {
-					ret++;
-					break;
-				}
-				current = current + l;
+			else if (a <= end && b > end) {
+				start = start + l;
+				end = start + l;
 				ret++;
 			}
 		}
