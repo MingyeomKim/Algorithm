0f24f5ee1dfc0825fe8775fb320852f6d667bb8a
MingyeomKim <mingyum119@naver.com>
2024-04-08 18:56:22 +0900

    [240408] 7490 0 만들기

--- 백준/Brute-Force/7490 0 만들기.cpp
+++ 백준/Brute-Force/7490 0 만들기.cpp
@@ -1,81 +1,112 @@
 #include <iostream>
+#include <string>
 #include <vector>
 #include <algorithm>
-#include <map>
-#include <string>
-using namespace std;
+using namespace std; 
 
+int n; 
+vector<string> numbers;
 vector<string> answer;
-int n;
 
-string generateAnswer(string symbol) {
-	string s = "";
-	for (int i = 1; i < n; i++) {
-		s += i + '0';
-		s += symbol[i - 1];
+/*
+* 1 2 3 4 5
+* 1  +
+* 2  +
+* 3 4 -
+* 5
+* 결과 계산
+* 1 + 2 +
+*/
+
+int calculate(vector<string>& v, vector<char>& symbol) {
+	int sum = stoi(v[0]);
+	for (int i = 0; i < symbol.size(); i++) {
+		if (symbol[i] == '+') {
+			sum += stoi(v[i + 1]);
+		}
+		else if (symbol[i] == '-') {
+			sum -= stoi(v[i + 1]);
+		}
 	}
-	s += (n + '0');
-	return s;
+	return sum;
 }
 
-void solve(int number, int sum, string& symbol) {
-	if (number == n + 1 && sum == 0) {
-		answer.push_back(generateAnswer(symbol));
-		return;
+string createAnswer(vector<string>& v, vector<char>& symbol) {
+	string answer = ""; 
+	answer.push_back(v[0][0]);
+	for (int i = 1; i < v[0].length(); i++) {
+		answer.push_back(' ');
+		answer.push_back(v[0][i]);
 	}
-	if (number >= n + 1) {
+
+	for (int i = 0; i < symbol.size(); i++) {
+		answer.push_back(symbol[i]);
+		
+		string s = v[i + 1];
+		answer.push_back(s[0]);
+		for (int i = 1; i < s.length(); i++) {
+			answer.push_back(' ');
+			answer.push_back(s[i]);
+		}
+	}
+	return answer;
+}
+
+void solve(int index, vector<string> & v, vector<char>&  symbol) {
+	if (index == n) {
+		int sum  = calculate(v, symbol);
+		if (sum == 0) {
+			answer.push_back(createAnswer(v, symbol));
+		}
 		return;
 	}
 
+	string number = numbers[index];
+	// + number
+	v.push_back(number); 
 	symbol.push_back('+');
-	solve(number + 1, sum + number, symbol);
+	solve(index + 1, v, symbol); 
+	v.pop_back(); 
 	symbol.pop_back();
 
+	// - number
+	v.push_back(number);
 	symbol.push_back('-');
-	solve(number + 1, sum - number, symbol);
+	solve(index + 1, v, symbol);
+	v.pop_back();
 	symbol.pop_back();
-
-
-	symbol.push_back(' ');
-
-	string adding = "";
-	adding.push_back(number + '0');
-	int j = number - 1;
-	for (; j >= 1; j--) {
-		if (symbol[j - 1] != ' ') {
-			break;
-		}
-		else {
-			adding.push_back(j + '0');
-		}
-	}
-	reverse(adding.begin(), adding.end());
-
-	int next_sum = stoi(adding);
-	char symb = symbol[j - 1];
-	if (symb == '+') {
-		next_sum = sum + next_sum;
-	}
-	else if (symb == '-') {
-		next_sum = sum - next_sum;
-	}
-
-	solve(number + 1, next_sum, symbol);
-	symbol.pop_back();
+	
+	// ' ' number
+	string &back = v.back();
+	back.append(number);
+	solve(index + 1, v, symbol);
+	back.pop_back();
 }
 
 int main() {
-	int t; cin >> t;
-	while (t--) {
-		answer.clear();
-		cin >> n;
-		string symbol = "";
-		solve(2, 1, symbol);
-		sort(answer.begin(), answer.end());
+	ios_base::sync_with_stdio(false); 
+	cin.tie(NULL); 
+	cout.tie(NULL);
 
+	int t; cin >> t; 
+	while (t--) {
+		numbers.clear(); 
+		answer.clear();
+
+		cin >> n;
+		for (int i = 1; i <= n; i++) {
+			numbers.push_back(to_string(i)); 
+		}
+		vector<string> v; 
+		v.push_back("1");
+		vector<char> symbol;
+		solve(1, v, symbol);
+
+		sort(answer.begin(), answer.end());
 		for (string s : answer) {
 			cout << s << endl;
 		}
 		cout << endl;
 	}
+	return 0;
 }
