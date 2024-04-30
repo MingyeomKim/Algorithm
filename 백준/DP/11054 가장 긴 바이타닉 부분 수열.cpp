c29808b6eecf2cd4634891ea3a11c2cfb0665f48
MingyeomKim <mingyum119@naver.com>
2024-04-30 19:06:44 +0900

    [240430] 11054 가장 긴 증가하는 바이토닉 부분 수열

--- 백준/DP/11054 가장 긴 바이타닉 부분 수열.cpp
+++ 백준/DP/11054 가장 긴 바이타닉 부분 수열.cpp
@@ -1,44 +1,39 @@
 #include <iostream>
 using namespace std;
 
+int f_dp[1001];
+int l_dp[1001];
 int number[1001];
-
-int dp[1001][2];
 
 int main() {
 	int n; cin >> n;
-	for (int i = 0; i < n; i++) {
+	for (int i = 1; i <= n; i++) {
 		cin >> number[i];
 	}
 
-	dp[0][0] = 1;
-	for (int i = 1; i < n; i++) {
-		dp[i][0] = 1;
-		if (number[i] > number[i - 1]) {
-			dp[i][0] = dp[i - 1][0] + 1;
+	for (int i = 1; i <= n; i++) {
+		f_dp[i] = 1;
+		for (int j = 1; j <= i; j++) {
+			if (number[i] > number[j] && f_dp[i] < f_dp[j] + 1) {
+				f_dp[i] = f_dp[j] + 1;
+			}
 		}
 	}
-
-	dp[n - 1][1] = 1;
-	for (int i = n - 2; i >= 0; i--) {
-		dp[i][1] = 1;
-		if (number[i] > number[i + 1]) {
-			dp[i][1] = dp[i + 1][1] + 1;
+	for (int i = n; i >= 1; i--) {
+		l_dp[i] = 1;
+		for (int j = n; j >= i; j--) {
+			if (number[i] > number[j] && l_dp[i] < l_dp[j] + 1) {
+				l_dp[i] = l_dp[j] + 1;
+			}
 		}
 	}
 
 	int ret = 0;
-	for (int i = 0; i < n; i++) {
-		if (ret < dp[i][0] + dp[i][1] - 1) {
-			ret = dp[i][0] + dp[i][1] - 1;
+	for (int i = 0; i <= n; i++) {
+		if (ret < f_dp[i] + l_dp[i] - 1) {
+			ret = f_dp[i] + l_dp[i] - 1;
 		}
 	}
 	cout << ret << endl;
 	return 0;
 }
-
-//10 20 30 25 20 
-//1  2   3  1  1
-//1  1   3  2  1
-//
-//1  2   5  2  2
