73190cafe9ce89a2a75174203e159a2177fb77f6
MingyeomKim <mingyum119@naver.com>
2024-07-18 20:00:28 +0900

    [240718] 10836 여왕벌

--- 백준/Brute-Force/10836 여왕벌.cpp
+++ 백준/Brute-Force/10836 여왕벌.cpp
@@ -1,9 +1,6 @@
 #include <iostream>
-#include <cstring>
 #include <vector>
 using namespace std;
-
-int add[701][701];
 
 int main() {
 	ios_base::sync_with_stdio(false);
@@ -13,38 +10,28 @@ int main() {
 	int m, n;
 	cin >> m >> n;
 
+	vector<int> v(2 * m - 1, 1);
 	for (int i = 0; i < n; i++) {
-		int grow = 0;
-		vector<int> v; // 0, 0, 1, 1, 1, 2, 2
-		for (int j = 0; j < 3; j++) {
-			int cnt; cin >> cnt;
-			for (int k = 0; k < cnt; k++) {
-				v.push_back(grow);
-			}
-			grow++;
+		int zero, one, two; 
+		cin >> zero >> one >> two; 
+		
+		for (int i = zero; i < zero + one; i++) {
+			v[i]++;
 		}
 
-		int index = 0;
-		for (int i = m - 1; i >= 0; i--) {
-			add[i][0] += v[index];
-			index++;
-		}
-
-		for (int j = 1; j <= m - 1; j++) {
-			add[0][j] += v[index];
-			index++;
-		}
-	}
-
-	for (int j = 1; j < m; j++) {
-		for (int i = 1; i < m; i++) {
-			add[i][j] = add[0][j];
+		for (int i = zero + one; i < zero + one + two; i++) {
+			v[i] += 2;
 		}
 	}
 
 	for (int i = 0; i < m; i++) {
 		for (int j = 0; j < m; j++) {
-			cout << 1 + add[i][j] << " ";
+			if (i == 0 || j == 0) {
+				cout << v[abs(m - 1 - i) + j] << " ";
+			}
+			else {
+				cout << v[m - 1 + j] << " ";
+			}
 		}
 		cout << endl;
 	}
