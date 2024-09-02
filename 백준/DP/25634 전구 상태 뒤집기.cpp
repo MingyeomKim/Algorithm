0c0ba51f4a1d339dbe8c27d9ff8ea6139aa1a9f9
김민겸 <mingyum119@naver.com>
2024-09-02 12:08:42 +0900

    [240902] 25634 전구 상태 뒤집기 (실패)

--- 백준/DP/25634 전구 상태 뒤집기.cpp
+++ 백준/DP/25634 전구 상태 뒤집기.cpp
@@ -1,66 +1,39 @@
 #include <iostream>
-#define INF 987654321
 using namespace std;
 
+int a[200001];
+bool b[200001];
 int dp[200001];
-int light[200001];
 
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-
 	int n; cin >> n;
+	for (int i = 1; i <= n; i++) {
+		cin >> a[i];
+	}
+
 	int sum = 0;
 	for (int i = 1; i <= n; i++) {
-		cin >> light[i];
+		cin >> b[i];
+		if (b[i]) {
+			sum += a[i];
+		}
 	}
 
-	int max = -INF; 
-	int min = INF;
-	int max_index = 0;
-	int min_index = 0;
+	int maxValue;
 	for (int i = 1; i <= n; i++) {
-		int on; cin >> on;
-		if (on == 1) {
-			sum += light[i];
-			light[i] = -light[i];
-		}
-		if (i == 1) {
-			dp[i] = light[i];
+		if (b[i]) {
+			dp[i] = -a[i];
+			dp[i] = max(dp[i], dp[i - 1] - a[i]);
 		}
 		else {
-			dp[i] = dp[i - 1] + light[i];
+			dp[i] = a[i];
+			dp[i] = max(dp[i], dp[i - 1] + a[i]);
 		}
-
-		if (max < dp[i]) {
-			max = dp[i];
-			max_index = i;
-		}
-
-		if (min > dp[i]) {
-			min = dp[i];
-			min_index = i;
+		if (i == 1 || dp[i] > maxValue) {
+			maxValue = dp[i];
 		}
 	}
 
-	// min_index 와 오른쪽 중에서 제일 큰 것
-	int max_right = -INF;
-	for (int i = min_index + 1; i <= n; i++) {
-		if (max_right < dp[i]) {
-			max_right = dp[i];
-		}
-	}
-
-	// max_index 와 왼쪽 중에서 제일 큰 것
-	int min_left = INF;
-	for (int i = 0; i < max_index; i++) {
-		if (min_left > dp[i]) {
-			min_left = dp[i];
-		}
-	}
-	
-	int gap = std::max(max_right - min, max - min_left);
-	cout << sum + gap << endl;
+	cout << sum + maxValue << endl;
 	return 0;
 }
